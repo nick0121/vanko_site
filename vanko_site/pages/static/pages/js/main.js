9b21bf328c76d85102417ae0f7f4047b4c4e7e64
nick hartford <skyler0121@gmail.com>
2020-08-15 20:46:23 -0400

    header

--- vanko_site/pages/static/pages/js/main.js
+++ vanko_site/pages/static/pages/js/main.js
@@ -2,4 +2,19 @@
 
 function hamburgerToggle(x) {
     x.classList.toggle("change");
+
+    let drop = document.getElementById('dropMenu');
+    if (drop.style.top === "") {
+        drop.style.top = 60 + 'px';
+    } else if (drop.style.top === "-154px") {
+        drop.style.top = 60 + 'px';
+    } else if (drop.style.top === "0px") {
+        drop.style.top = -154 + 'px';
+    } else if (drop.style.top === "60px") {
+        drop.style.top = -154 + 'px';
+    }
+
 };
+
+
+
