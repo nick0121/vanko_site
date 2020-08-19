1244c88fd489af44a87933ff53474bf2d561195b
nick hartford <skyler0121@gmail.com>
2020-08-18 20:14:35 -0400

    header toggle working, need to fix overlap

--- vanko_site/pages/static/pages/js/main.js
+++ vanko_site/pages/static/pages/js/main.js
@@ -4,8 +4,15 @@ function hamburgerToggle(x) {
     x.classList.toggle("change");
 
     //Code to toggle dropdown menu
+    let menu = document.getElementById('dropMenu');
 
-
+    if (menu.classList.contains('retracted')) {
+        menu.classList.remove('retracted');
+        menu.classList.add('extended');
+    } else {
+        menu.classList.remove('extended');
+        menu.classList.add('retracted');
+    }
 
 };
 
